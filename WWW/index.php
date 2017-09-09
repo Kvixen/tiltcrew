2f491acc917b8a94dcf8a827faf0095ee669fca6
Emil Wallentin <Emil.wallentin@gmail.com>
2017-09-09 22:22:26 +0200

    Skiss är klar

--- WWW/index.php
+++ WWW/index.php
@@ -9,20 +9,28 @@
         <link rel="stylesheet" type="text/css" href="css/main.css">
         <script type="text/javascript" src="script/main.js"></script>
     </head>
+    <body>
     <?php
     include "include/menu.php";
     ?>
     
-    
+    <br>
     <div class="content">
         <div id="left">
-        
+            <div>
+            
+            </div>
         </div>
         <div id="middle">
-        
+            <div>
+            
+            </div>
         </div>
         <div id="right">
-        
+            <div>
+            
+            </div>
         </div>
     </div>
+    </body>
 </html>
