660487aea1f55eb3f51f27114722d7153922be5b
Emil Wallentin <Emil.wallentin@gmail.com>
2017-12-15 14:34:42 +0100

    Trying to upload 2.0 again

---
+++ WWW/include/navbar.php
@@ -0,0 +1,42 @@
+<nav class="SelfNavbar navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+        <a class="SelfBrand navbar-brand" href="#">
+        <img src="assets/TILT.png" width="40px" height="25px" class="d-inline-block align-top" alt="">
+            TILT Crew
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="SelfLinks navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+                <a class="SelfDropdown nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Games</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Destiny 2</a>
+                    <a class="dropdown-item" href="#">Overwatch</a>
+                    <a class="dropdown-item" href="#">League of Legends</a>
+                    <a class="dropdown-item" href="#">World of Warcraft</a>
+                    <a class="dropdown-item" href="#">Counter Strike: Global Offensive</a>
+                    <a class="dropdown-item" href="#">Player Unknown's Battlegrounds</a>
+                </div>
+            </li>
+            <li class="SelfLink nav-item">
+                <a class="nav-link" href="#">Social</a>
+            </li>
+            <li class="SelfLink nav-item">
+                <a class="nav-link" href="#">Join us</a>
+            </li>
+            <li class="SelfLink nav-item">
+                <a class="nav-link" href="#">Members</a>
+            </li>
+            <li class="SelfLink nav-item">
+                <a class="nav-link" href="#">About us</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search..." aria-label="Search">
+            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        </div>
+    </div>
+</nav>
---
+++ WWW/profile.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <?php include('include/navbar.php'); ?>
+        <div class="container-fluid">
+            <div class="row SelfTopBuffer">
+                <div class="col-lg">
+                    <div class="row">
+                        <div class="col-lg-"></div>
+                        <div class="col-lg- SelfContent"></div>
+                        <div class="col-lg-"></div>
+                    </div>
+                </div>
+                <div class="col-lg-8 SelfContent">
+                        <img src="https://www.w3schools.com/css/paris.jpg" alt="..." class="img-thumbnail">
+                </div>
+                <div class="col-lg">
+                    <div class="row">
+                        <div class="col-lg-"></div>
+                        <div class="col-lg- SelfContent"></div>
+                        <div class="col-lg-"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
